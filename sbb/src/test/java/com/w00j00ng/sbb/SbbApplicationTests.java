3cb1ea64dedcdd0804d8938ee539342d5e53b1e5
w00j00ng <w00j00ng351@gmail.com>
2024-01-15 16:46:10 +0900

    feat: Start Spring Boot

---
+++ sbb/src/test/java/com/w00j00ng/sbb/SbbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w00j00ng.sbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
