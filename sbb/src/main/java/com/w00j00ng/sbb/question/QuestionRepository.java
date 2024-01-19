b64239d7690defac05a168f2ff62bf4deb3eb30f
용우중 <wjYong@openerd.com>
2024-01-19 09:21:48 +0900

    feat: pagning

--- sbb/src/main/java/com/w00j00ng/sbb/question/QuestionRepository.java
+++ sbb/src/main/java/com/w00j00ng/sbb/question/QuestionRepository.java
@@ -2,6 +2,8 @@ package com.w00j00ng.sbb.question;
 
 
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,4 +12,5 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubject(String subject);
     Question findBySubjectAndContent(String subject, String content);
     List<Question> findBySubjectLike(String subject);
+    Page<Question> findAll(Pageable pageable);
 }
