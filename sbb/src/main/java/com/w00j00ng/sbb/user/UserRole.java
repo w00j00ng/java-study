36b75c7d6276d7d6c95c0bcf1ea5d1058fc3889f
w00j00ng <w00j00ng351@gmail.com>
2024-01-23 17:42:48 +0900

    feat: account login, logout

---
+++ sbb/src/main/java/com/w00j00ng/sbb/user/UserRole.java
@@ -0,0 +1,15 @@
+package com.w00j00ng.sbb.user;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+    ADMIN("ROLE_ADMIN"),
+    USER("ROLE_USER");
+
+    UserRole(String value) {
+        this.value = value;
+    }
+
+    private String value;
+}
